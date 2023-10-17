3130185cb1d3baa0dff4a8f6600acea6da7c2a91
kvqn <guneetaggarwal@gmail.com>
2023-10-18 01:25:14 +0530

    python prediction api

--- model/server.py
+++ model/server.py
@@ -1,5 +1,17 @@
+from io import BytesIO
 from typing import Annotated
+from PIL import Image
 from fastapi import FastAPI, File
+import tensorflow as tf
+from tensorflow import keras
+import base64
+
+from model import CHARACTERS
+
+model = keras.models.load_model("model.keras")
+if model is None:
+    raise Exception("Model not found.")
+
 
 app = FastAPI()
 
@@ -9,8 +21,16 @@ async def root():
 
 
 @app.post("/predict")
-async def predict(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
+async def predict(file: Annotated[str, File()]):
+    if file.startswith('data:image/jpeg;base64,'):
+        file = file[len('data:image/jpeg;base64,'):]
+    base64_bytes = base64.b64decode(file)
+    image = Image.open(BytesIO(base64_bytes))
+    image = image.resize((256, 256))
+    image = tf.keras.utils.img_to_array(image)
+    image = tf.expand_dims(image, axis=0)
+    prediction = CHARACTERS[model.predict(image).argmax()]
+    return {"prediction": prediction}
 
 
 def start_server(args):
